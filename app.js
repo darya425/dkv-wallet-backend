c45c6a01acb126d47c9a6db84000ea0633da7a0c
Niko Yea <nikoyea14@gmail.com>
2021-12-15 18:55:50 +0100

    add corn job function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const https = require('https');
+const CronJob = require('cron').CronJob;
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
@@ -14,7 +17,8 @@ const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 const options = {
-  customCss: '.swagger-ui .model-box-control:focus, .swagger-ui .models-control:focus, .swagger-ui .opblock-summary-control:focus {outline: none}'
+  customCss:
+    '.swagger-ui .model-box-control:focus, .swagger-ui .models-control:focus, .swagger-ui .opblock-summary-control:focus {outline: none}',
 };
 
 app.use(logger(formatsLogger));
@@ -25,7 +29,11 @@ app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/transactions', transactionsRouter);
 app.use('/api/categories', categoriesRouter);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options),
+);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
@@ -35,5 +43,12 @@ app.use((err, req, res, next) => {
   const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
+
+const job = new CronJob('0 */25 6-20 * * *', function () {
+  const d = new Date();
+  https.get('https://wallet-g5.herokuapp.com/login');
+  console.log('Every 25 minutes between 6-20:', d);
+});
+job.start();
 
 module.exports = app;
